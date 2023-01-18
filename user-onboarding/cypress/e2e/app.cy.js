d4feb5ec0a340121dfc00fc961129085099a6af7
Colton Rhodenbaugh <coltonrhodenbaugh@Coltons-Air.hsd1.il.comcast.net>
2023-01-18 16:53:28 -0600

    got majority of tests done

--- user-onboarding/cypress/e2e/app.cy.js
+++ user-onboarding/cypress/e2e/app.cy.js
@@ -1,4 +1,63 @@
-//sanity check
-it("sanity-check",()=>{
+describe("User-Onboarding", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/");
+    })
+
+    //helpers
+    const textInput = () => cy.get("input[name=username]");
+    const emailInput = () => cy.get("input[name=email]");
+    const passInput = () => cy.get("input[name=password]");
+    const tosCheck = () => cy.get(`input[type="checkbox"]`);
+    const submitBtn = () => cy.get("input[type=submit]");
+
+    //sanity check
+    it("sanity-check",()=>{
     expect(7 + 11).to.equal(18);
+    })
+
+    it("the proper elements are showing", () => {
+        textInput().should("exist");
+        emailInput().should("exist");
+        passInput().should("exist");
+        tosCheck().should("exist");
+        submitBtn().should("exist");
+    })
+
+    describe("Filling out text", () => {
+        it("can navigate to site", () => {
+            cy.url().should("include", "localhost"); 
+        })
+        it("can type into inputs", () => {
+            textInput()
+            .should("have.value", "")
+            .type("Colton")
+            .should("have.value", "Colton");
+
+            emailInput()
+            .should("have.value", "")
+            .type("crhodenbaugh2204@gmail.com")
+            .should("have.value", "crhodenbaugh2204@gmail.com");
+
+            passInput()
+            .should("have.value", "")
+            .type("foobar")
+            .should("have.value", "foobar");
+        })
+        describe("checkbox", () => {
+
+        })
+        describe("submit button is working", () => {
+          
+        })
+
+
+
+    })
+
+
+
+
+
+
+
 })
